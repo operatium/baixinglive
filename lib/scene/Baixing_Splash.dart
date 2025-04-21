18918fe2d7dc3603302e98f52c3199ccc3b0f16c
operatium <operatium@163.com>
2025-04-21 12:28:36 +0800

    闪屏页布局

--- lib/scene/Baixing_Splash.dart
+++ lib/scene/Baixing_Splash.dart
@@ -9,21 +9,41 @@ class Baixing_SplashScene extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width: 360.w,
-        height: 640.w,
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("images/baixing_girl_xxxl.jpg"),
-            fit: BoxFit.contain,
+            image: AssetImage("images/baixing_bg_jianbian.png"),
+            fit: BoxFit.fill,
           ),
         ),
         child: Stack(
           children: [
             Center(
-              child: Container(
+              child: SizedBox(
                 width: 80.w,
                 height: 82.w,
                 child: Image.asset("images/baixing_logo.webp"),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: IntrinsicHeight(
+                child: Container(
+                  margin: EdgeInsets.only(bottom: 20.w),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 77.w,
+                        height: 20.w,
+                        margin: EdgeInsets.only(bottom: 5.w),
+                        child: Image.asset("images/baixing_99zhibo_text.webp"),
+                      ),
+                      Text(
+                        "与喜欢的你不期而遇",
+                        style: TextStyle(color: Color(0xFF999999), fontSize: 8.sp),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ],
